d3122ed61c4658235f7366bc9c2800087320593e
Lucho <luisalejandrolee@gmail.com>
2018-07-21 12:53:19 -0500

    Removed useless test function "print_other_truth" which was included for testing git setup. Sent "print_truth" function to the end of file

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -1,24 +1,5 @@
 devtools::use_package("data.table")
 
-
-#' A test function that prints "Lucho es el mejor!" by default,
-#' but can also print someone else's name
-#' @param name Name of the person who rox
-#' @return String saying who rox
-#' @usage By default, it prints the truth, but you can change \code{name}
-#' to be nice to someone else
-print_truth <- function(name = "Lucho"){
-  print(paste0(name, " es el mejor!"))
-  dt <- data.table::data.table(a = name, b = "es el mejor!")
-  print(dt)
-}
-
-print_something <- function(){
-  print("something")
-}
-# Random comments to test git
-
-
 #' Input NAs in a data.table with the column's median, mean or zeros
 #' @param dt Data table
 #' @param cols Columns from \code{dt}
@@ -70,3 +51,15 @@ input_nas <- function(dt, cols, treatment) {
     ) #lapply
    ] # dt
 } #end
+
+#' A test function that prints "Lucho es el mejor!" by default,
+#' but can also print someone else's name
+#' @param name Name of the person who rox
+#' @return String saying who rox
+#' @usage By default, it prints the truth, but you can change \code{name}
+#' to be nice to someone else
+print_truth <- function(name = "Lucho"){
+  print(paste0(name, " es el mejor!"))
+  dt <- data.table::data.table(a = name, b = "es el mejor!")
+  print(dt)
+}
